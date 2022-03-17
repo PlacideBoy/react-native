eb6fcb41ab5ade209a961df38876174063f306b4
Jorge Lucas <83237763+PlacideBoy@users.noreply.github.com>
2022-03-16 21:05:59 -0300

    declaração de variáveis

--- AULA02/onebithealth/src/components/Form/index.js
+++ AULA02/onebithealth/src/components/Form/index.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import {View, Text, TextInput, Button} from "react-native"
+import ResultImc from "./ResultImc/";
 
 
 
 export default function Form() {
+
+const [height, setHeight] = useState(null)
+const [weight, setWeight] = useState(null)
+const [messageImc, setMessageImc] = useState("preencha o peso e altura")
+const [imc, setImc] = useState(null)
+const [textButton, setTextButton] = useState("Calcular")
+
     return(
         <View>
             <View>

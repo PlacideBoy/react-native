8e3877df71e9b1d5af05196283513b65b64a88c0
Jorge Lucas <83237763+PlacideBoy@users.noreply.github.com>
2022-04-10 12:52:33 -0300

    set final de camera
    Configurações finais da API câmera para renderizar as fotos e trocar de câmera

--- AULA06/myCamera/App.js
+++ AULA06/myCamera/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal} from 'react-native';
 import { Camera } from 'expo-camera';
 import { useState, useEffect, useRef } from 'react';
 import { FontAwesome } from "@expo/vector-icons";
@@ -9,6 +9,7 @@ export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back)
   const [hasPermission, setHasPermission] = useState(null)
   const [capturedPhoto, setCapturedPhoto] = useState(null)
+  const [open, setOpen] = useState(false)
 
   useEffect (() => {
     (async () => {
@@ -29,6 +30,7 @@ export default function App() {
     if(camRef){
       const data = await camRef.current.takePictureAsync();
       setCapturedPhoto(data.uri)
+      setOpen(true)
     }
   }
 
@@ -37,6 +39,7 @@ export default function App() {
       <Camera
       style={styles.camera}
       type={type}
+      ref={camRef}
       >
         <View style={styles.contentButtons}>
           <TouchableOpacity
@@ -59,6 +62,23 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Camera>
+      {capturedPhoto &&(
+        <Modal
+        animationType='slide'
+        transparent={true}
+        visible={open}
+        >
+          <View style={styles.contentModal}>
+            <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() =>{setOpen(false)}}
+            >
+              <FontAwesome name='close' size={50} color='#fff'></FontAwesome>
+            </TouchableOpacity>
+              <Image style={styles.imgPhoto} source={{uri: capturedPhoto}}></Image>
+            </View>
+        </Modal>
+)}
     </SafeAreaView>
   );
 }
@@ -100,5 +120,21 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     borderRadius: 50,
+  },
+  contentModal: {
+    flex:1,
+    justifyContent:"center",
+    alignItems:"flex-end",
+    margin:20,
+  },
+  closeButton: {
+    position:"absolute",
+    top:10,
+    left:2,
+    margin:10,
+  },
+  imgPhoto: {
+    width:"100%",
+    height:400,
   }
 });
